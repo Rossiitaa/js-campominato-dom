378cd758c43ce3becf91df62a1666f5b7bd1fd20
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-24 13:42:35 +0200

    template reconstruction (with problem)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,84 @@
 const buttonPlay = document.getElementById('start-game')
 
-    const dinamicGrid = document.getElementById('grid-dinamic');
-    
-    const selectionButton = document.getElementById('diff-button');
-
-
     buttonPlay.addEventListener('click', function(){
+        createGame();
+    });
+
+    function createGame() {
+
+    const dinamicGrid = document.getElementById('grid-dinamic');
+
+    dinamicGrid.innerHTML = '';
+
+    let diffLevel = parseInt(document.getElementById("diff-button").value);
+    
+    let numberCell;
+    let rowCell;
+
+    switch (diffLevel) {
+        default:
+            numberCell = 100;
+        break;
+        case 1:
+            numberCell = 81;
+        break;
+        case 2:
+            numberCell = 49;
+        break;
+    }
+
+    rowCell = Math.sqrt(numberCell);
+
+    for (let i = 0; i <= numberCell; i++) {
+        const newBox = document.createElement('div');
+        newBox.classList.add('square');
+
+        const boxCalc = `calc(100 / ${rowCell})`
+        newBox.style.width = boxCalc;
+        newBox.style.height = boxCalc;
+
+        newBox.innerHTML = i;
+
+        dinamicGrid.append(newBox);
+    }
+    console.log(diffLevel, numberCell, rowCell)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /*  buttonPlay.addEventListener('click', function(){
         dinamicGrid.innerHTML = null;
+
         let val = document.getElementById("diff-button").value;
+        
+
     if(val === "0") {
         for (let i = 0; i < 100; i++) {
 
@@ -16,8 +87,8 @@ const buttonPlay = document.getElementById('start-game')
 
             newBox.addEventListener('click' , function(){
 
-                newBox.classList.toggle('azzurro');
-                console.log(newBox.innerHTML);
+            newBox.classList.toggle('light-blue');
+            console.log(newBox.innerHTML);
             });
 
             newBox.innerHTML = i;
@@ -33,8 +104,8 @@ const buttonPlay = document.getElementById('start-game')
 
             newBox.addEventListener('click' , function(){
 
-                newBox.classList.toggle('azzurro');
-                console.log(newBox.innerHTML);
+            newBox.classList.toggle('light-blue');
+            console.log(newBox.innerHTML);
             });
 
             newBox.innerHTML = i;
@@ -50,8 +121,8 @@ const buttonPlay = document.getElementById('start-game')
 
             newBox.addEventListener('click' , function(){
 
-                newBox.classList.toggle('azzurro');
-                console.log(newBox.innerHTML);
+            newBox.classList.toggle('light-blue');
+            console.log(newBox.innerHTML);
             });
 
             newBox.innerHTML = i;
@@ -65,4 +136,6 @@ function createNewBox() {
     const currentBox = document.createElement('div');
     currentBox.classList.add('square');
     return currentBox;
-}
+} */
+
+ 
