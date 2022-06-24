69c0e1ef3d2b547eb8a87d38c1fbf3c189379567
Rossiitaa <rosario.scuotto@outlook.it>
2022-06-24 17:39:42 +0200

    problem solved but continue with yesterday's code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const buttonPlay = document.getElementById('start-game')
+/* const buttonPlay = document.getElementById('start-game')
 
     buttonPlay.addEventListener('click', function(){
         createGame();
@@ -29,11 +29,11 @@ const buttonPlay = document.getElementById('start-game')
 
     rowCell = Math.sqrt(numberCell);
 
-    for (let i = 0; i <= numberCell; i++) {
+    for (let i = 0; i < numberCell; i++) {
         const newBox = document.createElement('div');
         newBox.classList.add('square');
 
-        const boxCalc = `calc(100 / ${rowCell})`
+        const boxCalc = `calc(100% / ${rowCell})`
         newBox.style.width = boxCalc;
         newBox.style.height = boxCalc;
 
@@ -42,100 +42,66 @@ const buttonPlay = document.getElementById('start-game')
         dinamicGrid.append(newBox);
     }
     console.log(diffLevel, numberCell, rowCell)
-    }
+    } */
 
 
 
 
 
+const buttonPlay = document.getElementById('start-game')
+
+const dinamicGrid = document.getElementById('grid-dinamic');
+    
+const selectionButton = document.getElementById('diff-button');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   /*  buttonPlay.addEventListener('click', function(){
-        dinamicGrid.innerHTML = null;
-
-        let val = document.getElementById("diff-button").value;
+buttonPlay.addEventListener('click', function(){
+    dinamicGrid.innerHTML = null;
+    let val = document.getElementById("diff-button").value;
         
-
     if(val === "0") {
         for (let i = 0; i < 100; i++) {
-
             const newBox = createNewBox();
             newBox.classList.add('rs_box_consegna');
-
             newBox.addEventListener('click' , function(){
-
             newBox.classList.toggle('light-blue');
             console.log(newBox.innerHTML);
             });
-
             newBox.innerHTML = i;
             dinamicGrid.append(newBox);
         }
     }
-
     if(val === "1") {
         for (let i = 0; i < 81; i++) {
-
             const newBox = createNewBox();
             newBox.classList.add('rs_box_consegna_int');
-
             newBox.addEventListener('click' , function(){
-
             newBox.classList.toggle('light-blue');
             console.log(newBox.innerHTML);
             });
-
             newBox.innerHTML = i;
             dinamicGrid.append(newBox);
         }
     }
-
     if(val === "2") {
         for (let i = 0; i < 49; i++) {
-
             const newBox = createNewBox();
             newBox.classList.add('rs_box_consegna_diff');
-
             newBox.addEventListener('click' , function(){
-
             newBox.classList.toggle('light-blue');
             console.log(newBox.innerHTML);
             });
-
             newBox.innerHTML = i;
             dinamicGrid.append(newBox);
         }
     }
-
 });
 
+
+
+/* functions */
 function createNewBox() {
     const currentBox = document.createElement('div');
     currentBox.classList.add('square');
     return currentBox;
-} */
-
- 
+} 
